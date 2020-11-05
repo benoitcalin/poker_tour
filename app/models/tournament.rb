c185a139d0c111a0c1446bc23e35104aee2618ed
Benoit Calin <benoit.calin@gmail.com>
2020-11-05 13:45:54 +0100

    Associations

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,2 +1,5 @@
 class Tournament < ApplicationRecord
+  has_many :participations
+  has_many :sessions
+  has_many :games, through: :sessions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  has_many: :participations
+  has_many: :tournaments, through: :participations
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
 end
