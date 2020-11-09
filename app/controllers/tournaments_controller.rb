4129da26adcbfbdcaf27640701e0e1dea10f95a3
Benoit Calin <benoit.calin@gmail.com>
2020-11-09 20:47:17 +0100

    Add column sorting

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,6 +1,6 @@
 class TournamentsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
-
+  helper_method :sort_column, :sort_direction
 
   def index
     @tournaments = Tournament.all
@@ -9,8 +9,12 @@ class TournamentsController < ApplicationController
   def show
     allis = ['Magrande', 'Mougne69', 'Elcasador', 'Geo8535', 'jukebox91210', 'ritouns', 'StevOLRRR', 'leotom7576', 'KensBanker', 'Chaton96', 'Sakorh']
     @tournament = Tournament.find(params[:id])
-    if params[:query] === "allis"
+    if params[:query] && params["sort"]
+      @stats = sortable_column_order(@tournament.tournament_results.includes(:player)).select { |tr| allis.include?(tr.player.name) }
+    elsif params[:query]
       @stats = @tournament.tournament_results.includes(:player).select { |tr| allis.include?(tr.player.name) }
+    elsif params["sort"]
+      @stats = sortable_column_order(@tournament.tournament_results.includes(:player))
     else
       @stats = @tournament.tournament_results.includes(:player)
     end
@@ -34,5 +38,21 @@ class TournamentsController < ApplicationController
 
   def tournament_params
     params.require(:tournament).permit(:name)
+  end
+
+  def sortable_column_order(base)
+    base.order("#{sort_column} #{sort_direction}")
+  end
+
+  def sort_direction
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : nil
+  end
+
+  def sort_column
+    authorize_sortable_column.include?(params[:sort]) ? params[:sort] : "earnings"
+  end
+
+  def authorize_sortable_column
+    ["games", "average_position", "reentries", "bets", "earnings", "net_earnings", "earnings_by_game"]
   end
 end
